54f859a74e740a01ab10a11aecc168d54f5c87a1
Erin Omcikus <erinomcikus@gmail.com>
2023-01-07 18:11:29 -0500

    broken word

--- react-app/src/components/Books/AllBooks/allbooks.js
+++ react-app/src/components/Books/AllBooks/allbooks.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { getBooks } from '../../../store/books';
 import BookCard from './bookcard';
 import './allbooks.css'
@@ -8,13 +8,13 @@ const AllBooks = () => {
     const dispatch = useDispatch()
     const books = useSelector(state => state.books.allBooks)
     const booksArray = Object.values(books)
-    const user = useSelector(state=> state.session.user)
+    const user = useSelector(state => state.session.user)
     useEffect(() => {
         dispatch(getBooks())
     }, [dispatch,])
 
-        return (
-            <>
+    return (
+        <>
             <div className='allbooks-container'>
 
                 {booksArray.map(book => (
@@ -22,17 +22,15 @@ const AllBooks = () => {
                         <BookCard key={book.id} book={book} />
                     </div>
                 ))}
-                {!user && (
-                    <div></div>
-                )}
+
             </div>
             {user && (
                 <button><NavLink to='/books/add' className='book-button'>Create a Book</NavLink></button>
             )}
-            
-</>
-        )
-    }
 
-    
+        </>
+    )
+}
+
+
 export default AllBooks
