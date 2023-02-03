2e0e9f8fe3526f724d7151f9926e5cb794500e0c
Erin Omcikus <erinomcikus@gmail.com>
2023-02-03 10:15:13 -0500

    trying to match shelves with ids

--- app/api/shelf_routes.py
+++ app/api/shelf_routes.py
@@ -4,19 +4,22 @@ from ..forms import shelf_form
 from flask_login import login_required, current_user
 from .auth_routes import validation_errors_to_error_messages
 
-shelf_routes = Blueprint('shelves', __name__)
+shelf_routes = Blueprint('/shelves', __name__)
 
 @shelf_routes.route('/')
-def get_shelf_by_user():
-    # shelves = Shelf.query.
-    # return {'shelves': [shelf.to_dict() for shelf in shelves]}
-    pass
-
-@shelf_routes.route('/user')
 @login_required
-def get_shelves_of_current_user():
+def get_shelf_by_user():
+    """get books on shelf by user"""
+    print('------------------', current_user.id)
     shelves = Shelf.query.filter(Shelf.user_id == current_user.id)
-    return {'shelves': [shelf.to_dict() for shelf in shelves]}
+    singleShelf = [shelf.to_dict() for shelf in shelves]
+    print('AAAAAAAAAAAAAA', singleShelf)
+    books = Book.query.filter(Book.shelf_id == singleShelf)
+
+    return  [book.to_dict() for book in books]
+
+
+
 
 # @shelf_routes.routes('/add')
 # @login_required
