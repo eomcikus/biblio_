f4dc7483efa3bcfa60647d05e7d2836a9d39b159
Erin Omcikus <erinomcikus@gmail.com>
2023-02-09 13:42:21 -0500

    redirect on delete book

--- react-app/src/components/shelves/DeleteShelf.js
+++ react-app/src/components/shelves/DeleteShelf.js
@@ -5,13 +5,14 @@ import { deleteBookFromShelf } from "../../store/shelves";
 
 const DeleteBFromShelf = ({bookId}) => {
     const dispatch = useDispatch()
-
+    const history = useHistory()
     const books = useSelector(state=> state.shelves.shelves.books)
     const book = books.find(book => book.bookId === bookId) 
 
     const handleDelete = (e) => {
         e.preventDefault()
         dispatch(deleteBookFromShelf(bookId))
+        history.push('/shelves/user')
     }
     return (
        <div><button onClick={handleDelete}>Delete</button></div> 
